3f3574b6fc231b46cd8b77ae7c00da06f01c7446
AndresFelipeMoreno <andres.moreno07@uptc.edu.co>
2022-10-09 17:16:40 -0500

    PRUEBAS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,52 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('El valor varCadena debe ser igual a un valor especifico: Esta es una cadena', () => {
+    const appComponent = new AppComponent();
+    const valor = appComponent.varCadena;
+    expect(valor).toEqual('Esta es una cadena');
   });
 
-  it(`should have as title 'UnitTest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('UnitTest');
-  });
+  it('Una cadena debe contener el valor de varPalabra', ()=>{
+    const appComponent = new AppComponent();
+    const valor = appComponent.varPalabra;
+    expect('Esta es una palabra').toContain(valor);
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('UnitTest app is running!');
-  });
+  it('Debe retornar True si es mayor de edad, 18 años o mas', ()=>{
+    const appComponent = new AppComponent();
+    const respuesta = appComponent.isAdult(18);
+    expect(respuesta).toBeTruthy();
+  })
+
+  it('Debe retornar False si es menor de edad, menos de 18 años', ()=>{
+    const appComponent = new AppComponent();
+    const respuesta = appComponent.isAdult(17);
+    expect(respuesta).toBeFalsy();
+  })
+
+  it('Debe retornar Fizz si es divisible por 3', ()=>{
+    const appComponent = new AppComponent();
+    const respuesta = appComponent.fizzBuzz(3);
+    expect(respuesta).toEqual('Fizz');
+  })
+
+  it('Debe retornar Buzz si es divisible por 5', ()=>{
+    const appComponent = new AppComponent();
+    const respuesta = appComponent.fizzBuzz(5);
+    expect(respuesta).toEqual('Buzz');
+  })
+
+  it('Debe retornar FizzBuzz si es divisible por 3 y 5', ()=>{
+    const appComponent = new AppComponent();
+    const respuesta = appComponent.fizzBuzz(15);
+    expect(respuesta).toEqual("FizzBuzz");
+  })
+
+  it('Debe retornar el mismo numero si no es divisible ni por 3 ni por 5', ()=>{
+    const appComponent = new AppComponent();
+    const respuesta = appComponent.fizzBuzz(4);
+    expect(respuesta).toEqual(4);
+  })
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isAdult(age: number) {
+    if (age >= 18){
+      return true;
+    }
+      return false;
+  }
+
+  fizzBuzz(number:number){
+    if((number%3 == 0) && (number%5 == 0)){
+      return "FizzBuzz";
+    }
+    if(number%3 == 0){
+      return "Fizz";
+    }
+    if(number%5 == 0){
+      return "Buzz";
+    }
+    return number;
+  }
+
   title = 'UnitTest';
+  varCadena = 'Esta es una cadena';
+  varPalabra = "Esta";
 }
+
